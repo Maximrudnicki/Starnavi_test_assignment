fbe6a939db9bcc81573f559ad548090b46367dc2
Maxim Rudnitskiy <rydnizkui@gmail.com>
2024-10-17 16:44:35 +0300

    User model updated and Post model added

--- models/post.py
+++ models/post.py
@@ -1,7 +1,18 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
 
 from config.db import Base
 
 
 class Post(Base):
-    __tablename__ = 'posts'
+    __tablename__ = "posts"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(Text, nullable=True)
+    text: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.now(), onupdate=datetime.now())
+    author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+
+    user: Mapped["User"] = relationship("User", back_populates="posts")
--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from config.db import Base
+from models.post import Post
 
 
 class User(Base):
@@ -11,6 +12,7 @@ class User(Base):
     email: Mapped[str] = mapped_column(unique=True, index=True)
     password: Mapped[str]
     banned: Mapped[bool] = mapped_column(default=False)
-
     auto_reply_enabled: Mapped[bool] = mapped_column(default=True)
     auto_reply_delay: Mapped[int] = mapped_column(default=3600)
+
+    posts: Mapped[list["Post"]] = relationship("Post", back_populates="user")
